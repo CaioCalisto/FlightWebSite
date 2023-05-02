696f09b35ffa2dc5c57093b14c9cd6e7eb168b67
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-02 22:12:35 +0100

    fixed: container fluid

--- flight-web-site/src/App.tsx
+++ flight-web-site/src/App.tsx
@@ -8,17 +8,17 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   
   return (
-    <>
+    <Container fluid>
       <Navbar sticky="top" style={{ padding: "0", margin: "0", width: "100%" }}>
         <Header />
       </Navbar>
       <Routes>
-         <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
       <Navbar fixed="bottom">
         <Footer />
       </Navbar>
-    </>
+    </Container>
   )
 }
 

90bc7046535c13fcc3d30129ea5f0b0573de1205
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-02 10:32:08 +0100

    added responsive logo

--- flight-web-site/src/components/Header.tsx
+++ flight-web-site/src/components/Header.tsx
@@ -1,11 +1,15 @@
-import './Header.css'
+import "./Header.css";
 
 function Header() {
   return (
     <>
-      <h1>Header</h1>
+    <section className="logotipo-section">
+      <figure className="logotipo">
+        <img src="src/assets/ACME-logo.png" alt="" width="150" height="150" />
+      </figure>
+    </section>
     </>
-  )
+  );
 }
 
 export default Header;

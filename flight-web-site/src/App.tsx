90ecc5b762be2b5f8c7475bc38f484001ceb3b35
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-02 09:02:20 +0100

    created simple css layout

--- flight-web-site/src/App.tsx
+++ flight-web-site/src/App.tsx
@@ -1,10 +1,18 @@
-import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
-    <div>Hello</div>
+    <>
+      <header className="header">
+        <h1>HEADER</h1>
+      </header>
+      <main className="original-content">
+        <h1>CONTENT</h1>
+      </main>
+      <footer className="footer">
+        <h1>FOOTER</h1>
+      </footer>
+    </>
   )
 }
 

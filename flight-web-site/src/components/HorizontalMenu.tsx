126e47ddae663fc49517cf628483085b6fc36bef
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-03 10:21:00 +0100

    clean

--- flight-web-site/src/components/HorizontalMenu.tsx
+++ flight-web-site/src/components/HorizontalMenu.tsx
@@ -10,23 +10,12 @@ function HorizontalMenu() {
       className="d-flex justify-content-center"
       style={{ width: "100%", padding: "1em 1em 1em 1em", backgroundColor: "rgb(218, 212, 212)" }}
     >
+      {/* > 768px */}
       <span>&#9992; Flights</span>
       <div className="vr" />
       <span>&#128665; Car Hire</span>
       <div className="vr" />
       <span>&#127977; Hotels</span>
-      {/* > 768px */}
-      {/* <Row className="align-items-center">
-        <Col>
-          <span>&#9992; Flights</span>
-        </Col>
-        <Col>
-          <span>&#128665; Car Hire</span>
-        </Col>
-        <Col>
-          <span>&#127977; Hotels</span>
-        </Col>
-      </Row> */}
     </Stack>
   );
 }

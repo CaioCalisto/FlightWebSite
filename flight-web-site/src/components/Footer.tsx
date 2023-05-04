ebd748fe1adf9d1631e9ddb29db22e69398e9e1e
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-04 11:37:34 +0100

    Responsive text

--- flight-web-site/src/components/Footer.tsx
+++ flight-web-site/src/components/Footer.tsx
@@ -1,10 +1,9 @@
-import { Grid, Paper } from '@mui/material'
+import { Grid, Paper, Typography } from '@mui/material'
 import { Stack } from '@mui/system'
 import React from 'react'
 
 function Footer() {
   return (
-    
     <Grid container spacing={2} justifyContent="space-evenly">
       <Grid item>
         <Stack>
@@ -13,24 +12,30 @@ function Footer() {
         </Stack>
       </Grid>
       <Grid item>
-        <h1>Usefull Information</h1>
-        <h2>Destination Guide</h2>
-        <h2>Flight status</h2>
-        <h2>Baggage service</h2>
-        <h2>Accessibility</h2>
-        <h2>Site map</h2>
+        <Stack>
+          <h1>Usefull Information</h1>
+          <h2>Destination Guide</h2>
+          <h2>Flight status</h2>
+          <h2>Baggage service</h2>
+          <h2>Accessibility</h2>
+          <h2>Site map</h2>
+        </Stack>
       </Grid>
       <Grid item>
-        <h1>Corporate</h1>
-        <h2>About us</h2>
-        <h2>Careers</h2>
-        <h2>Investor relations</h2>
+        <Stack>
+          <h1>Corporate</h1>
+          <h2>About us</h2>
+          <h2>Careers</h2>
+          <h2>Investor relations</h2>
+        </Stack>
       </Grid>
       <Grid item>
-        <h1>Transparency</h1>
-        <h2>Contitions of carriage</h2>        
-        <h2>Passengers rights</h2>
-        <h2>Personal Data Policy</h2>
+        <Stack>
+          <h1>Transparency</h1>
+          <h2>Contitions of carriage</h2>        
+          <h2>Passengers rights</h2>
+          <h2>Personal Data Policy</h2>
+        </Stack>
       </Grid>
     </Grid>
   )

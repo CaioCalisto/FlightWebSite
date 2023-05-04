a250aa86eeee32d8bd6ff5806cce4b39f90a1438
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-04 12:08:00 +0100

    icon size

--- flight-web-site/src/components/Header.tsx
+++ flight-web-site/src/components/Header.tsx
@@ -5,12 +5,12 @@ function Header() {
   return (
     <Grid container>
       <Grid item xs={6}>
-        <img src="src/assets/ACME-logo.png" style={{ width: "30%"}}/>
+        <img src="src/assets/ACME-logo.png" style={{ width: "25%"}}/>
       </Grid>
       <Grid item xs={6}>
         <Stack direction="row" spacing={1} justifyContent="end">
-          <img src="src/assets/earth.png" alt="" style={{ width: "15%"}} />
-          <img src="src/assets/user.png" alt="" style={{ width: "15%"}} />
+          <img src="src/assets/earth.png" alt="" style={{ width: "10%"}} />
+          <img src="src/assets/user.png" alt="" style={{ width: "10%"}} />
         </Stack>
       </Grid>
     </Grid>

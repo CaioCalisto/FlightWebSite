11d55d8ba0260952a40dc5583a8be2ad6b1556f5
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-07 23:13:30 +0100

    improved font size

--- flight-web-site/src/components/Footer.tsx
+++ flight-web-site/src/components/Footer.tsx
@@ -1,44 +1,36 @@
-import { Grid, Paper, Typography } from '@mui/material'
-import { Stack } from '@mui/system'
-import React from 'react'
+
+import { Stack } from "@mui/system";
+import React from "react";
 
 function Footer() {
   return (
-    <Grid container spacing={2} justifyContent="space-evenly">
-      <Grid item>
-        <Stack>
-          <h1>On the web</h1>
-          <h2>Twitter</h2>
-        </Stack>
-      </Grid>
-      <Grid item>
-        <Stack>
-          <h1>Usefull Information</h1>
-          <h2>Destination Guide</h2>
-          <h2>Flight status</h2>
-          <h2>Baggage service</h2>
-          <h2>Accessibility</h2>
-          <h2>Site map</h2>
-        </Stack>
-      </Grid>
-      <Grid item>
-        <Stack>
-          <h1>Corporate</h1>
-          <h2>About us</h2>
-          <h2>Careers</h2>
-          <h2>Investor relations</h2>
-        </Stack>
-      </Grid>
-      <Grid item>
-        <Stack>
-          <h1>Transparency</h1>
-          <h2>Contitions of carriage</h2>        
-          <h2>Passengers rights</h2>
-          <h2>Personal Data Policy</h2>
-        </Stack>
-      </Grid>
-    </Grid>
-  )
+    <Stack direction="row" spacing={2} justifyContent="space-evenly">
+      <Stack direction="column">
+        <h1>On the web</h1>
+        <h2>Twitter</h2>
+      </Stack>
+      <Stack direction="column">
+        <h1>Usefull Information</h1>
+        <h2>Destination Guide</h2>
+        <h2>Flight status</h2>
+        <h2>Baggage service</h2>
+        <h2>Accessibility</h2>
+        <h2>Site map</h2>
+      </Stack>
+      <Stack direction="column">
+        <h1>Corporate</h1>
+        <h2>About us</h2>
+        <h2>Careers</h2>
+        <h2>Investor relations</h2>
+      </Stack>
+      <Stack direction="column">
+        <h1>Transparency</h1>
+        <h2>Contitions of carriage</h2>
+        <h2>Passengers rights</h2>
+        <h2>Personal Data Policy</h2>
+      </Stack>
+    </Stack>
+  );
 }
 
-export default Footer
+export default Footer;

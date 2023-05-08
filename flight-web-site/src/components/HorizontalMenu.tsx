faeee780c6589253b25d10c8751d33ba95b2b3ff
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-08 14:15:56 +0100

    ...

--- flight-web-site/src/components/HorizontalMenu.tsx
+++ flight-web-site/src/components/HorizontalMenu.tsx
@@ -1,22 +1,36 @@
-import { Stack } from '@mui/system'
-import React from 'react'
-import { Link } from 'react-router-dom'
-import AirplanemodeActiveIcon from '@mui/icons-material/AirplanemodeActive';
-import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
-import HotelIcon from '@mui/icons-material/Hotel';
+import { Stack } from "@mui/system";
+import React from "react";
+import { Link } from "react-router-dom";
+import AirplanemodeActiveIcon from "@mui/icons-material/AirplanemodeActive";
+import DirectionsCarIcon from "@mui/icons-material/DirectionsCar";
+import HotelIcon from "@mui/icons-material/Hotel";
 
 function HorizontalMenu() {
   return (
-    <Stack direction={"row"} spacing={4} justifyContent="center" alignItems="center">
+    <Stack
+      direction={"row"}
+      spacing={6}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Stack direction="row" alignItems="end">
         <Link to="/flights">
-          <AirplanemodeActiveIcon />Flight
-        </Link>  
-        <Link to="/carhire">
-          <DirectionsCarIcon/> Car Hire
+          <AirplanemodeActiveIcon />
+          Flight
         </Link>
-        <HotelIcon /> Hotel
+      </Stack>
+      <Stack direction="row" alignItems="end">
+        <Link to="/carhire">
+          <DirectionsCarIcon />
+          Car Hire
+        </Link>
+      </Stack>
+      <Stack direction="row" alignItems="end">
+        <HotelIcon />
+        Hotel
+      </Stack>
     </Stack>
-  )
+  );
 }
 
-export default HorizontalMenu
+export default HorizontalMenu;

25c829d3be998c6c0f5bd7261a53e1d7f0beca51
Caio Calisto <caiocesarcalisto@gmail.com>
2023-05-09 10:23:37 +0100

    simplify imports

--- flight-web-site/src/layouts/PublicLayout.tsx
+++ flight-web-site/src/layouts/PublicLayout.tsx
@@ -1,11 +1,11 @@
 import { Container, Divider, useMediaQuery } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import HorizontalMenu from "../components/navs/HorizontalMenu";
-import SideBarMenu from "../components/navs/SideBarMenu";
-import SmallFooter from "../components/SmallFooter";
+import Footer from "@components/Footer";
+import Header from "@components/Header";
+import HorizontalMenu from "@components/navs/HorizontalMenu";
+import SideBarMenu from "@components/navs/SideBarMenu";
+import SmallFooter from "@components/SmallFooter";
 
 function PublicLayout() {
   const medium = useMediaQuery("(min-width:768px)");
